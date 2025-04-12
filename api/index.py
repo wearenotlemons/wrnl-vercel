ad375be99572082b524b7708843e7e1bd5e932cd
wearenotlemons <kenneth.kohkl+lemons@gmail.com>
2025-04-12 03:49:41 +0000

    Modified for vercel

---
+++ api/index.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template, jsonify
+import requests
+from urllib.parse import quote
+import os
+
+app = Flask(__name__, template_folder="..")  # Assuming index.html is in root
+
+SCRIPT_URL = os.environ.get("SCRIPT_URL")
+
+@app.route("/", methods=["GET", "POST"])
+def handle():
+    if request.method == "GET":
+        return render_template("index.html")
+
+    name = request.form.get("nameInput")
+    if not name:
+        return render_template("index.html", msg="Please enter a name.")
+
+    data = get_data(name)
+    if data:
+        name = data.get("name")
+        sessions = ", ".join(map(str, data.get("sessions", [])))
+        totalHours = data.get("totalHours")
+        if totalHours > 0:
+            return render_template("index.html", name=name, sessions=sessions, totalHours=totalHours)
+        else:
+            return render_template("index.html", name=name, msg="You did not attend any sessions.")
+    else:
+        return render_template("index.html", msg="Failed to retrieve data. Please try again.")
+
+def get_data(name):
+    encoded_name = quote(name)
+    try:
+        print("Fetching data from Google Apps Script...")
+        response = requests.get(f"{SCRIPT_URL}?name={encoded_name}")
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print("❌ Error fetching data:", e)
+        return None
